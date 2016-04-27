7e438991bedf8deb424142465156d4fdda3f6919
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-04-27 15:20:56 +0200

    J'avais totalement oublier cette histoire de define dans Grace, du coup j'ai mis le main dans une macro et je l'appelle evidement.

--- 01/Grace.c
+++ 01/Grace.c
@@ -1,3 +1,4 @@
 #include <stdio.h>
 /* COMMENT */
-int main(){FILE *file = fopen("Grace_kid.c", "w");char*a="#include <stdio.h>%c/* COMMENT */%cint main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(file, a, 10, 10, 34, 34, 34, 34, 34, a, 34, 10);fclose(file);return(0);}%c";fprintf(file, a, 10, 10, 34, 34, 34, 34, 34, a, 34, 10);fclose(file);return(0);}
+#define F(x) int main(){FILE *file = fopen("Grace_kid.c", "w");char*a="#include <stdio.h>%c/* COMMENT */%c#define F(x) int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(file, a, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);fclose(file);return(0);}%cF(xxxx)%c";fprintf(file, a, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);fclose(file);return(0);}
+F(xxxx)
