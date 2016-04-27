1ec5a7b54fa74ae3dc1e1cf7e45f5ac18d4240bd
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-04-27 15:55:15 +0200

    les 2 derniers defines inutiles qui me permet juste de respecter la consigne ....

--- 01/Grace.c
+++ 01/Grace.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
 /* COMMENT */
-#define F(x) int main(){FILE *file = fopen("Grace_kid.c", "w");char*a="#include <stdio.h>%c/* COMMENT */%c#define F(x) int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(file, a, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);fclose(file);return(0);}%cF(xxxx)%c";fprintf(file, a, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);fclose(file);return(0);}
+#define OPEN FILE*file=fopen
+#define CLOSE fclose(file)
+#define F(x) int main(){OPEN("Grace_kid.c", "w");char*a="#include <stdio.h>%c/* COMMENT */%c#define OPEN FILE*file=fopen%c#define CLOSE fclose(file)%c#define F(x) int main(){OPEN(%cGrace_kid.c%c, %cw%c);char*a=%c%s%c;fprintf(file, a, 10, 10, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);CLOSE;return(0);}%cF(xxxx)%c";fprintf(file, a, 10, 10, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);CLOSE;return(0);}
 F(xxxx)
