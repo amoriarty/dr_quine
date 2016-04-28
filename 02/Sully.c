da647538e3659da03af25cfebb8cf977ea1e0dc5
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-04-28 16:25:16 +0200

    J'ai deja une premier ebauche de ce que dois faire mon programme. Maintenant la problematique va etre de le faire se duplique lui meme, en changean un variable sur la route.

--- 02/Sully.c
+++ 02/Sully.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void							newfile(int i)
-{
-	char 						*file_name = (char *)malloc(sizeof(char) * 10);
-	FILE						*file = NULL;
-
-	file_name[9] = 0;
-	sprintf(file_name, "Sully_%d.c", i);
-	file = fopen(file_name, "w");
-	fprintf(file, "Hello World\n");
-}
+#define STR "int main(){return(0);}"
+#define NEWSTR(size) (char *)malloc(sizeof(char) * size)
 
 int								main(void)
 {
 	int 						i = 5;
-
-	while (i >= 0)
-		newfile(i--);
+	char						*file_name = NEWSTR(10);
+	char 						*exec_name = NEWSTR(8);
+	char 						*command = NEWSTR(54);
+	while (i >= 0) {
+		sprintf(file_name, "Sully_%i.c", i);
+		sprintf(exec_name, "Sully_%i", i);
+		FILE						*file = fopen(file_name, "w");
+		fprintf(file, STR);
+		fclose(file);
+		sprintf(command, "clang -Wall -Wextra -Werror -o %s %s && ./%s", exec_name, file_name, exec_name);
+		system(command);
+		i--;
+	}
 	return(0);
 }
